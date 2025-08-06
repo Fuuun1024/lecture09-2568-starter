7c53cf98e5beb3f343b96c88d4d4835562d2eef9
Fuuun1024 <worapopfong@gmail.com>
2025-08-06 11:39:20 +0700

    close import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import App from "./pages/CounterPage";
+//import App from "./pages/CounterPage";
 import { router } from "./routers";
 
 createRoot(document.getElementById("root")!).render(
